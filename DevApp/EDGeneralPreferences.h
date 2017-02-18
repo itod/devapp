9943ae14822851ef04cd2833a23be65641dfa1b0
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2017-02-18 17:19:45 -0600

    * themes/prefs

---
+++ DevApp/EDGeneralPreferences.h
@@ -0,0 +1,13 @@
+//
+//  EDGeneralPreferences.h
+//  Editor
+//
+//  Created by Todd Ditchendorf on 11/13/10.
+//  Copyright 2010 Todd Ditchendorf. All rights reserved.
+//
+
+#import <IDEKit/IDEBasePreferences.h>
+
+@interface EDGeneralPreferences : IDEBasePreferences
+
+@end
---
+++ DevApp/EDGeneralPreferences.m
@@ -0,0 +1,39 @@
+//
+//  EDGeneralPreferences.m
+//  Editor
+//
+//  Created by Todd Ditchendorf on 11/13/10.
+//  Copyright 2010 Todd Ditchendorf. All rights reserved.
+//
+
+#import "EDGeneralPreferences.h"
+#import <TDAppKit/TDViewController.h>
+
+@interface EDGeneralPreferences ()
+
+@end
+
+@implementation EDGeneralPreferences
+
+- (void)dealloc {
+
+    [super dealloc];
+}
+
+
+#pragma mark -
+#pragma mark NSWindowController
+
+- (void)awakeFromNib {
+
+}
+
+
+#pragma mark -
+#pragma mark Actions
+
+
+#pragma mark -
+#pragma mark Private
+
+@end
