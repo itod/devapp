f3777e5b46bd369c679d0e43d7cd4f07e9bf3f56
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2017-03-21 22:12:30 -0500

    * leak

--- DevApp/EDMemoryCodeRunner.m
+++ DevApp/EDMemoryCodeRunner.m
@@ -32,7 +32,7 @@ void PerformOnMainThread(void (^block)(void)) {
 }
 
 @interface EDMemoryCodeRunner ()
-@property (nonatomic, assign) id <EDCodeRunnerDelegate>delegate; // weakref
+@property (assign) id <EDCodeRunnerDelegate>delegate; // weakref
 @property (nonatomic, retain) XPInterpreter *interp;
 @property (nonatomic, retain) TDInterpreterSync *debugSync;
 @property (nonatomic, copy) NSString *identifier;
@@ -66,6 +66,7 @@ void PerformOnMainThread(void (^block)(void)) {
     self.delegate = nil;
     self.interp = nil;
     self.debugSync = nil;
+    self.identifier = nil;
 
     dispatch_release(_controlThread), _controlThread = NULL;
     dispatch_release(_executeThread), _executeThread = NULL;
@@ -74,11 +75,6 @@ void PerformOnMainThread(void (^block)(void)) {
     self.stdErrPipe = nil;
     
     [super dealloc];
-}
-
-
-- (void)setDelegate:(id<EDCodeRunnerDelegate>)d {
-    _delegate = d;
 }
 
 
@@ -405,6 +401,7 @@ void PerformOnMainThread(void (^block)(void)) {
 #pragma mark Notifications
 
 - (void)canvasDidUpdate:(NSNotification *)n {
+    TDAssertExecuteThread();
     [self fireDelegateDidUpdate:nil];
 }
 
