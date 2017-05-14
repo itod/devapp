031558e5bb848570ac213ef708adf7b741bded56
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2017-05-14 14:30:18 -0500

    * stroke with 4 args

--- DevApp/lang/FNStroke.m
+++ DevApp/lang/FNStroke.m
@@ -24,10 +24,21 @@
     XPFunctionSymbol *funcSym = [XPFunctionSymbol symbolWithName:[[self class] name] enclosingScope:nil];
     funcSym.nativeBody = self;
     
-    XPSymbol *color = [XPSymbol symbolWithName:@"color"];
-    funcSym.orderedParams = [NSMutableArray arrayWithObjects:color, nil];
+    XPSymbol *r = [XPSymbol symbolWithName:@"r"];
+    XPSymbol *b = [XPSymbol symbolWithName:@"b"];
+    XPSymbol *g = [XPSymbol symbolWithName:@"g"];
+    XPSymbol *a = [XPSymbol symbolWithName:@"a"];
+    funcSym.orderedParams = [NSMutableArray arrayWithObjects:r, g, b, a, nil];
+    funcSym.defaultParamObjects = [NSMutableDictionary dictionaryWithObjectsAndKeys:
+                                   [XPObject number:0.0], @"g",
+                                   [XPObject number:0.0], @"b",
+                                   [XPObject number:1.0], @"a",
+                                   nil];
     funcSym.params = [NSMutableDictionary dictionaryWithObjectsAndKeys:
-                      color, @"color",
+                      r, @"r",
+                      g, @"g",
+                      b, @"b",
+                      a, @"a",
                       nil];
     
     return funcSym;
@@ -35,12 +46,23 @@
 
 
 - (XPObject *)callWithWalker:(XPTreeWalker *)walker functionSpace:(XPMemorySpace *)space argc:(NSUInteger)argc {    
-    XPObject *colorObj = [space objectForName:@"color"]; TDAssert(colorObj);
-
-    NSColor *c = [self asColor:colorObj];
+    XPObject *rObj = [space objectForName:@"r"]; TDAssert(rObj);
     
     CGContextRef ctx = [self.canvasGraphicsContext graphicsPort];
-    CGContextSetRGBStrokeColor(ctx, [c redComponent], [c greenComponent], [c blueComponent], [c alphaComponent]);
+    if (1 == argc) {
+        NSColor *c = [self asColor:rObj];
+        CGContextSetRGBStrokeColor(ctx, [c redComponent], [c greenComponent], [c blueComponent], [c alphaComponent]);
+    } else {
+        XPObject *gObj = [space objectForName:@"g"]; TDAssert(gObj);
+        XPObject *bObj = [space objectForName:@"b"]; TDAssert(bObj);
+        XPObject *aObj = [space objectForName:@"a"]; TDAssert(aObj);
+        
+        double r = rObj.doubleValue;
+        double g = gObj.doubleValue;
+        double b = bObj.doubleValue;
+        double a = aObj.doubleValue;
+        CGContextSetRGBStrokeColor(ctx, r, g, b, a);
+    }
     
     return nil;
 }
