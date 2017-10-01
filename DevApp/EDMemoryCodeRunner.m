99ea1595b0119d9ecf5d376466f5e4db3ef3e36f
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2017-10-01 15:38:50 -0500

    * cu

--- DevApp/EDMemoryCodeRunner.m
+++ DevApp/EDMemoryCodeRunner.m
@@ -636,6 +636,7 @@ void TDPerformAfterDelay(dispatch_queue_t q, double delay, void (^block)(void)) 
     [i declareNativeFunction:[FNLine class]];
     [i declareNativeFunction:[FNBezier class]];
     
+    TDAssert(i.globals);
     [i.globals setObject:[XPObject number:M_PI] forName:@"PI"];
     [i.globals setObject:[XPObject number:M_PI_2] forName:@"HALF_PI"];
     [i.globals setObject:[XPObject number:M_PI_4] forName:@"QUARTER_PI"];
